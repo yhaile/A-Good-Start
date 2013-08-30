4e25d5e837c4cdef07b52d03e6721d93a8c2f208
Yonas B Haile <yhaile@t220b-38163-02.matc.madison.login>
2013-08-30 12:43:12 -0500

    Initial Commit

---
+++ A Good Start/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  A Good Start
+//
+//  Created by Yonas B Haile on 8/30/13.
+//  Copyright (c) 2013 Yonas B Haile. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
